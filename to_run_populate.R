fde30abf6e69497719fabc4499ef60aaf4729c83
Davide <davide.messina@ars.toscana.it>
2025-01-13 16:51:34 +0100

    Create to_run where schema is populated

---
+++ to_run_populate.R
@@ -0,0 +1,10 @@
+# source(here::here("inception database.R"))
+# source(here::here("steps.R"))
+# source(here::here("codebooks.R"))
+source(here::here("populate index file.R"))
+# source(here::here("variables.R"))
+# source(here::here("parameters.R"))
+# source(here::here("configuration files.R"))
+# source(here::here("dag.R"))
+source(here::here("schema and pipeline.R"))
+
