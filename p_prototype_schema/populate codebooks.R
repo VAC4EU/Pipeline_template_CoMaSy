b0c4b463e07344a95df02b6cd9316100fac58f68
Davide Messina <davide.messina@ars.toscana.it>
2025-01-24 20:12:56 +0100

    More robust variable selection, support for multiple example sheets and shell table discovery

--- p_prototype_schema/populate codebooks.R
+++ p_prototype_schema/populate codebooks.R
@@ -1,7 +1,7 @@
 add_dm_properties <- function(x, real_cb_datamodel) {
   
   dataModel_properties <- list()
-  dataModel_properties[["varname"]] <- real_cb_datamodel[x, VarName] # string
+  dataModel_properties[["varname"]] <- real_cb_datamodel[x, Varname] # string
   dataModel_properties[["description"]] <- real_cb_datamodel[x, Description] # string
   dataModel_properties[["format"]] <- real_cb_datamodel[x, Format] # string
   dataModel_properties[["vocabulary"]] <- real_cb_datamodel[x, Vocabulary] # string
@@ -43,7 +43,7 @@ add_parameters_properties <- function(x, real_cb_parameters, real_cb_datamodel) 
   parameters_properties <- list()
   parameters_properties[["name"]] <- real_cb_parameters[x, parameter.in.the.variable.name] # string
   # TODO change string to array 
-  parameters_properties[["parameterVariable"]] <- as.list(real_cb_datamodel[real_cb_datamodel[, Parameters] %in% parameters_properties[["name"]], VarName]) # string
+  parameters_properties[["parameterVariable"]] <- as.list(real_cb_datamodel[real_cb_datamodel[, Parameters] %in% parameters_properties[["name"]], Varname]) # string
   parameters_properties[["values"]][["items"]] <- real_cb_parameters[x, values] # string
   # TODO ask name of macro
   parameters_properties[["configFile"]] <- "" # string
@@ -69,26 +69,65 @@ add_examples_properties <- function(example_dfs) {
   
 }
 
+add_examples_properties <- function(example_dfs) {
+  
+  examples_properties <- list()
+  examples_properties[["inputDataset"]] <- example_dfs[1:length(example_dfs) - 1] # string
+  examples_properties[["outputDataset"]] <- example_dfs[length(example_dfs)] # string
+  
+  examples_items <- list()
+  examples_items[["properties"]] <- examples_properties
+  
+  return(examples_items)
+  
+}
+
+retrieve_examples <- function(sheet_name, wb) {
+  
+  is_empty_row <- data.table::data.table(openxlsx2::wb_to_df(wb, col_names = F, sheet = sheet_name))
+  is_empty_row <- data.table::data.table(!is.na(is_empty_row[, 1]))
+  setnames(is_empty_row, colnames(is_empty_row), "is_empty")
+  is_empty_row[, group_id := data.table::fifelse(is_empty != data.table::shift(is_empty, fill = F), 1, 0)]
+  is_empty_row[, group_id := cumsum(group_id)]
+  is_empty_row[, row_id := .I]
+  
+  example_dfs <- list()
+  for (grp in 1:is_empty_row[, max(group_id)]) {
+    if (isTRUE(unique(is_empty_row[group_id == grp, is_empty]))) {
+      tmp <- data.table::data.table(openxlsx2::wb_to_df(wb, col_names = F, sheet = sheet_name, rows = is_empty_row[group_id == grp, min(row_id)]:is_empty_row[group_id == grp, max(row_id)]))
+      example_dfs <- append(example_dfs, list(tmp))
+    }
+  }
+  
+  return(add_examples_properties(example_dfs))
+}
+
 # add_examples_properties(ori_file, inputDataset, outputDataset)
 
 add_codebook <- function(wb, example_dfs) {
   
   sheets_name <- openxlsx2::wb_get_sheet_names(wb)
+  real_cb_parameters <- data.table::data.table(openxlsx2::wb_to_df(wb, sheet = 3, check_names = T))
+  real_cb_datamodel <- data.table::data.table(openxlsx2::wb_to_df(wb, sheet = 2, check_names = T))
+  real_cb_metadata <- data.table::data.table(openxlsx2::wb_to_df(wb, sheet = 1, check_names = T))
+  index_file <- data.table::data.table(openxlsx2::wb_to_df(here::here("i_codebooks/00_index.xlsx"), sheet = 1, check_names = T))
   
-  real_cb_example <- data.table::data.table(xlsx::read.xlsx(excel_file, sheetIndex = 4))
-  real_cb_parameters <- data.table::data.table(openxlsx2::read_xlsx(excel_file, sheet = 3, check_names = T))
-  real_cb_datamodel <- data.table::data.table(xlsx::read.xlsx(excel_file, sheetIndex = 2))
-  real_cb_metadata <- data.table::data.table(xlsx::read.xlsx(excel_file, sheetIndex = 1))
-  index_file <- data.table::data.table(xlsx::read.xlsx(here::here("i_codebooks/00_index.xlsx"), sheetIndex = 1))
+  if ("medatata_name" %in% colnames(real_cb_metadata)) {
+    setnames(real_cb_metadata, "medatata_name", "metadata_name")
+  }
+  
+  if ("VarName" %in% colnames(real_cb_datamodel)) {
+    setnames(real_cb_datamodel, "VarName", "Varname")
+  }
   
   metadata_properties <- list()
-  metadata_properties[["description"]] <- real_cb_metadata[medatata_name == "Content of the dataset", metadata_content] # string
-  metadata_properties[["unitsOfObservation"]] <- real_cb_metadata[medatata_name == "Unit of observation", metadata_content] # string
-  metadata_properties[["codebookReference"]] <- real_cb_metadata[medatata_name == real_cb_metadata[medatata_name == "Dataset where the list of UoOs is fully listed and with 1 record per UoO", metadata_content], metadata_content] # string
-  metadata_properties[["observationsDescription"]] <- real_cb_metadata[medatata_name == "How many observations per UoO", metadata_content] # string
-  metadata_properties[["NxUoO"]] <- real_cb_metadata[medatata_name == "NxUoO", metadata_content] # string
+  metadata_properties[["description"]] <- real_cb_metadata[metadata_name == "Content of the dataset", metadata_content] # string
+  metadata_properties[["unitsOfObservation"]] <- real_cb_metadata[metadata_name == "Unit of observation", metadata_content] # string
+  metadata_properties[["codebookReference"]] <- real_cb_metadata[metadata_name == real_cb_metadata[metadata_name == "Dataset where the list of UoOs is fully listed and with 1 record per UoO", metadata_content], metadata_content] # string
+  metadata_properties[["observationsDescription"]] <- real_cb_metadata[metadata_name == "How many observations per UoO", metadata_content] # string
+  metadata_properties[["NxUoO"]] <- real_cb_metadata[metadata_name == "NxUoO", metadata_content] # string
   #TODO metadata_properties[["Variables capturing the UoO"]]
-  metadata_properties[["primaryKey"]] <- tstrsplit(real_cb_metadata[medatata_name == "Primary key", metadata_content], " ") # string
+  metadata_properties[["primaryKey"]] <- tstrsplit(real_cb_metadata[metadata_name == "Primary key", metadata_content], " ") # string
   metadata_properties[["dataModel"]][["items"]] <- lapply(1:nrow(real_cb_datamodel), function(x) {
     add_dm_properties(x, real_cb_datamodel)
   }) # object
@@ -98,16 +137,17 @@ add_codebook <- function(wb, example_dfs) {
   metadata_properties[["parameters"]][["items"]] <- lapply(1:nrow(real_cb_parameters), function(x) {
     add_parameters_properties(x, real_cb_parameters, real_cb_datamodel)
   }) # object
-  metadata_properties[["examples"]][["items"]] <- add_examples_properties(example_dfs) # object
+  metadata_properties[["examples"]][["items"]] <- lapply(sheets_name[grepl("(?i)example", openxlsx2::wb_get_sheet_names(wb))],
+                                                         retrieve_examples, wb) # object
   
   codebooks_items_properties <- list()
-  codebooks_items_properties[["name"]] <- real_cb_metadata[medatata_name == "Name of the dataset", metadata_content] # string
+  codebooks_items_properties[["name"]] <- real_cb_metadata[metadata_name == "Name of the dataset", metadata_content] # string
   codebooks_items_properties[["name"]][["pattern"]] <- "^D[2-6]_.*"
   
   index_file_filtered <- index_file[dataset_name == codebooks_items_properties[["name"]][[1]], ]
   codebooks_items_properties[["generatorStep"]] <- index_file_filtered[, step_producing_this_dataset] # string
   codebooks_items_properties[["metadata"]][["properties"]] <- metadata_properties # object
-  codebooks_items_properties[["shellTable"]] <- any(grepl("shell table", sheets_name)) # boolean
+  codebooks_items_properties[["shellTable"]] <- any(grepl("(?i)shell table", sheets_name)) # boolean
   rm(metadata_properties)
   
   codebooks_items <- list()
@@ -125,26 +165,7 @@ add_codebook <- function(wb, example_dfs) {
   
 }
 
-is_empty_row <- data.table::data.table(openxlsx2::wb_load(here::here("i_codebooks/D3_TD_condition.xlsx")) %>%
-  openxlsx2::wb_to_df(col_names = F, sheet = "Examples"))
-is_empty_row <- data.table::data.table(!is.na(is_empty_row[, 1]))
-setnames(is_empty_row, colnames(is_empty_row), "is_empty")
-is_empty_row[, group_id := data.table::fifelse(is_empty != data.table::shift(is_empty, fill = F), 1, 0)]
-is_empty_row[, group_id := cumsum(group_id)]
-is_empty_row[, row_id := .I]
 
-data.table::data.table(openxlsx2::wb_load(here::here("i_codebooks/D3_TD_condition.xlsx")) %>%
-                         openxlsx2::wb_to_df(sheet = "Examples", rows = 1:10))
-
-example_dfs <- list()
-for (grp in 1:is_empty_row[, max(group_id)]) {
-  if (isTRUE(unique(is_empty_row[group_id == grp, is_empty]))) {
-    tmp <- data.table::data.table(openxlsx2::wb_load(here::here("i_codebooks/D3_TD_condition.xlsx")) %>%
-                                    openxlsx2::wb_to_df(sheet = "Examples",
-                                                        rows = is_empty_row[group_id == grp, min(row_id)]:is_empty_row[group_id == grp, max(row_id)]))
-    example_dfs <- append(example_dfs, list(tmp))
-  }
-}
 
 all_codebooks <- setdiff(list.files(here::here("i_codebooks")), "00_index.xlsx")
 all_codebooks <- all_codebooks[!grepl("^~", all_codebooks)]
@@ -156,5 +177,5 @@ codebooks <- lapply(all_codebooks, function(x) {
   
   wb <- openxlsx2::wb_load(excel_file)
   
-  add_codebook(wb, example_dfs)
+  add_codebook(wb)
 })
